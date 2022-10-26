8278282d3839189ac1b1be8efeae9134462a24fb
laytoshcode <laytoshtwo@yahoo.com>
2022-10-25 21:57:13 -0700

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,21 +6,21 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+  int sign = 1;
+  unsigned int num = 0;
 
-	while (!('0' <= *s && <= '9') && *s != '\0')
+  while (!('0' <= *s && <= '9') && *s != '\0')
 	{
-		if (*s  == '-')
-			sign *= -1;
-		if (*s == '+')
-			sign *= +1;
-		s++;
+	  if (*s  == '-')
+		sign *= -1;
+          if (*s == '+')
+		sign *= +1;
+	  s++;
 	}
 	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
- 	       num = (num * 10) + (*s - '0');
-	       s++;
+ 	    num = (num * 10) + (*s - '0');
+	    s++;
 	}
 	return (num * sign);
 }
