b10b5eca5243e327c704cd15b5f0449507f29d70
laytoshcode <laytoshtwo@yahoo.com>
2022-10-28 16:51:50 -0700

    task1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
  *  @c:Varaiable text
  *  Return:Always 0
  */
-int_isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
@@ -12,6 +12,6 @@ int_isupper(int c)
 	}
 	else
 	{
- 		return (0);
+  		return (0);
 	}
 }	
