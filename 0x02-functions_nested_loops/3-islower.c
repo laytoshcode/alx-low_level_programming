6652d00b342fe451a239d5181789b75ba44cf172
laytoshcode <laytoshtwo@yahoo.com>
2022-10-30 13:44:24 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,10 @@
 * @c:character to check if it islowercase
 * Return:1 if lowercase,0 if not
 */
-int -islower(int c)
+int _islower(int c)
 {
-
      if (c >= 'a' && c <= 'z')
-
 	   return (1);
-
      else
-
 	  return (0);			
+}
