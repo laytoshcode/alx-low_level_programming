35bc3d6cf8120153f07a6a6aaaa5c41877b47f57
laytoshcode <laytoshtwo@yahoo.com>
2022-10-30 13:18:32 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
 * main -prints _putchar and a new line
-*  Return: Always 0 (Success)
+* Return: Always 0 
 */
 int main(void)
-	
-{      
-   
+{         
+	_putchar('-');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r'));
+	_putchar('r');
 	_putchar('\n');
 
-	 return (0);
+	return (0);
 }
-
