6e142af51c9c0bfc0b0362a9770d44f79a47a4de
laytoshcode <laytoshtwo@yahoo.com>
2022-10-30 14:32:13 -0700

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-  *_abs-computes  the absolute value of an integer.
- *   *@a:the integer to compute its absolute value.
-   *Return:returns the abs value 
-  */ 
-int _abs(int a)
+*abs-computes  the absolute value of an integer.
+*@a:the integer to compute its absolute value.
+*iReturn:returns the abs value 
+ */ 
+int _abs(int t)
 
 {
 	if (t >= 0)
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,22 @@
 #include "main.h"
 /**
- * jack_bauer-prints all minutes in a day from 00:00
-  * Return:0 if succeesful
-     *
-     * /
-   void jack_bauer(void)
-
-   {
-
-   	int i, j, k, m;
-
-
+*jack_bauer-prints all minutes in a day from 00:00
+* Return:0 if succeesful
+ */
+void jack_bauer(void)
+{
+int i, j, k, m;
 
 		for (i = 0; i <= 2; i++)
-
 			{
-                                 _putchar('0');
+                           _putchr('0');
 
-					for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 
-							{
+		{
 
-										for (k = 0; k <= 5; k++)
-
-													{
-
-																	for (m = 0; m <= 9; m++)
-
-																					{
+		for (k = 0; k <= 5; k++)                                                       
+		
+																
+		for (m = 0; m <= 9; m++)
+		{
