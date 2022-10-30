0a003d1b76b9f18daeeff18443f20d555454600c
laytoshcode <laytoshtwo@yahoo.com>
2022-10-30 14:03:17 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 # include "main.h"
-
 /**
  * print_sign-check the sign of a number
 *  @n:Number whose sign is to be checked
@@ -7,37 +6,20 @@
  *0 of number is zero
  *-1 if the number less than zero
       */
-
 int print_sign(int n)
-
 {
-
 	if (n > 0)
-
-	{
-
+         {	  
 	     _putchar('+');
-	     return (1);
-		
+	     return (1);	
 	 }   
-
-
           else if (n == 0)
-	  {
-
+         {
 	     _putchar('0');
-
-	       return (0);
-
-	  }
+	     return (0);
+	 }
 	else
-       {      
+         {      
 	       _putchar('-');
-
 	       	return (-1);
-
-				}
-
 }
-
-								}
