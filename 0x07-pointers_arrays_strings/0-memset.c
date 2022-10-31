d30c9f9d773a17aa39f0396588b1ca714942d0a2
laytoshcode <laytoshtwo@yahoo.com>
2022-10-31 12:34:15 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*simple_print_buffer - prints buffer in hexa
+*@buffer: the address of memory to print
+*@size: the size of the memory to print
+*Return: Nothing  
+*/
+*_memset(char *s,char b,unsigned int n)
+unsigned int a;
+for (a = 0;  a < n; a++)
+{
+	s[a] = b;
+}
+return (s);
