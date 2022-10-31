59c5248806f4e1760ee927b0560926bb45a35872
laytoshcode <laytoshtwo@yahoo.com>
2022-10-30 18:37:57 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+/**
+* ptint_numbers -prints 0123456789\n
+*
+* Return:void
+*/
+void print_numbers(void)
+{
+char c;
+for (c = '0';c c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
