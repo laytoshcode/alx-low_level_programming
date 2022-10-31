5a9c6d5ca700f3e0fa35254ac19e70bebdf77bf1
laytoshcode <laytoshtwo@yahoo.com>
2022-10-30 18:08:56 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include  "main.h"
+/**
+*-isdigit.c - checks for characters that are didgit
+*@c: character
+*
+* Return 0 or 1
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '0')
+return (1);
+else
+return (0);
+}
