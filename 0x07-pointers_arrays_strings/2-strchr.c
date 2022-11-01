ca2e55fd58ce1630f067d3761f206665fdc16bb7
laytoshcode <laytoshtwo@yahoo.com>
2022-10-31 21:27:12 -0700

    tasktwo

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  -strchr -Entry point
+ *  @s:input
+ *  @c:input
+ *  Return Always 0(Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i]>= '\0', char c)
+	{
+		if (s[i] == c)
+			return(&s[i]);
+	}
+	return (0);
+}
