56ec8c4c905595b290d5eeb5705a94760d8b970f
laytoshcode <laytoshtwo@yahoo.com>
2022-11-02 04:23:43 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,8 +22,8 @@ char *_strstr(char *haystack, char *needle);
 			do {
 				if (needle[index + 1] == '\0')
 					return (haystack);
-
 				index++;
+
 			} while (haystack[index] == needle[index]);
 		}
 
