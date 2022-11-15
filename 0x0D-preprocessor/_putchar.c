985b7a5df296b6d721eb5f4224210ec24817f37e
laytoshcode <laytoshtwo@yahoo.com>
2022-11-15 03:57:43 -0800

    task0

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,5 @@
+#include  <unistd.h>
+int _putchar.c(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,9 @@
+#ifndef Main_H
+#define Main_H
+#include <stdio>
+#include  "1-pi.h"
+#include  "2-main.c"
+#include  "3-function_like_macro.h"
+#include  "4-sum.h"
+
+#endif
