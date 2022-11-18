b3dd116630d324ec208803a202bc2821bf5c10df
laytoshcode <laytoshtwo@yahoo.com>
2022-11-18 09:30:16 -0800

    Beauty is variable

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,1 @@
-
-p_add -calculates the sum of two integers                                                                             
-
- * @: first integer                                                                                                       
- *
- *  * @b: second integer                                                                                                     
- *
- *   *                                                                                                                        
- *
- *    * Return; sum of a and b                                                                                                 
- *
- *     */                  
+     
