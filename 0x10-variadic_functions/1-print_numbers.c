3f684f63c0b6e893d766076491d5de12a2238cc5
laytoshcode <laytoshtwo@yahoo.com>
2022-11-18 23:23:11 -0800

    To be is to be the value

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
-i
 		if (index != (n-1) && separator != NULL)
 			printf("%s", separator);
 	}
