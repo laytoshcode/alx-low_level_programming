3d56fa689152e348707cbaf0c1629d5e552e355e
laytoshcode <laytoshtwo@yahoo.com>
2022-11-18 22:47:40 -0800

    To be is to be the value

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
  * print_numbers -orint numbers ,followed by a new line.
  * @seperators: The string to be printed between numbers.
