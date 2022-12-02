dc1ffc0c204c7b693b6963bbe1ea50bcd04fc81a
laytoshcode <laytoshtwo@yahoo.com>
2022-12-01 20:32:54 -0800

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,11 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: (cm-amos)Amos Mwongela
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,7 +14,4 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
-
-#endif
-
+int get_endi
