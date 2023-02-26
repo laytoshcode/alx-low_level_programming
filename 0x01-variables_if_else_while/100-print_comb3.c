91618b7f58ed57b3f42c3b66ca63518c409fff9c
laytoshcode <laytoshtwo@yahoo.com>
2023-02-26 15:02:25 -0800

    task100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,55 +1,41 @@
 #include <stdio.h>
 
-/*
- *main-program entry point.
- *   *Return:0 success, non zero fail
- */
+/**
+ * main - Program that print all possible different combinations of two digits.
+ *
+ * Numbers must be separated by ,, followed by a space.
+ * The two digits must be different.
+ * 01 and 10 are considered the same combination of the two digits 0 and 1.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
+ * You can only use the putchar function (every other function (printf,
+ * puts, etc…) is forbidden).
+ * You can only use putchar five times maximum in your code.
+ * You are not allowed to use any variable of type char.
+ * All your code should be in the main function.
+ *
+ * Return: 0.
+*/
 
-int main(void) {
+int main(void)
+{
+	int tens;
+	int ones;
 
-	 int i, k = '0', j;
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 
-
-
-	    for (i = '0'; i <= '9'; i++) {
-
-             for (j = k; j <= '9'; j++) {
-
-	        if (i != j) {
-
-	         char(i);
-
-	           putchar(j);
-         }
-                                             
-	               if (i == j) {
-
-                 
-		       continue;
-
-					          }
-
-		 if (i == '8' && j == '9') {
-    
-
-	        break;
-
-                } else 
-			
-	                                                                        
-			putchar(',');
-
-		        putchar(' ');
-                                                           
-												      }
-				      }
-
-			    k++;
-
-			      }
-
-	      putchar('\n');
-
-	        return (0);
-
+	return (0);
 }
