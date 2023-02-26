852a71f0475900f10f19344506c95a1a174644cb
laytoshcode <laytoshtwo@yahoo.com>
2023-02-26 15:51:58 -0800

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,24 @@
 #include "main.h"
+
 /**
- *print_most_numbers -prints
+ * print_most_numbers - Function tha prints numbers between 0 to 9,
+ * followed by a new line.
  *
- *Return :void
+ * Do not print 2 and 4.
+ * You can only use _putchar twice in your code.
+ *
+ * Return: no return.
  */
-void print _most_numbers(void)
+
+void print_most_numbers(void)
 {
-int a = 0;
-while (a < 10)
-{
-if (a != 2 && a != 4)
-_putchar(a + '0')
-a++;
-}
-_putchar('\n');
+
+int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		_putchar('0' + i);
+	}
+	_putchar('\n');
 }
