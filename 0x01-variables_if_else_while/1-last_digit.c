28bbac0d98a6c04647200c7750d334d2999a6c53
laytoshcode <laytoshtwo@yahoo.com>
2023-02-26 05:37:51 -0800

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,42 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
-*main-program entry point
-*Return:0 if no error, non-zero value if error 1-last_digit.c*/
-int main(void)
-{	
-	int n;
-	int a;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	a = n % 10;
-	if (a > 5)
+/**
+ * main - This program will assign a random number to the variable,
+ * n each time it is executed.
+ *
+ * The output of the program should be:
+ * The string Last digit of, followed by n, followed by,
+ * the string is, followed by:
+ * if the last digit of n is greater than 5: the string and is greater than 5,
+ * if the last digit of n is 0: the string and is 0,
+ * if the last digit of n is less than 6 and not 0: the string and is less,
+ * than 6 and not 0,
+ * followed by a new line.
+
+ * Return: 0
+*/
+
+int main(void)
 {
-		printf("Last digit of %d is %d and is greater than 5\n", n, a);
-		}
-	else if (a == 0)
-		{
-		printf("Last digit of %d is %d and is 0\n", n, a);
-		}
-	else if (a < 6 && a  != 0)
-		{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);				
-		}
-	return (0);
+    int n;
+    int last_digit;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    last_digit = n % 10;
+    if (last_digit > 5)
+    {
+        printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
+    }
+    else if (last_digit == 0)
+    {
+        printf("Last digit of %i is %d and is 0\n", n, last_digit);
+    }
+    else if (last_digit < 6 && last_digit != 0)
+    {
+        printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
+    }
+    return (0);
 }
