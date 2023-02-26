eab344a90beb5235b986e0414a593a8dd0d6ab33
laytoshcode <laytoshtwo@yahoo.com>
2023-02-26 14:55:43 -0800

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
+#include <ctype.h>
+
 /**
+ * main - Program that print all possible combinations of single-digit numbers.
  *
- *  *main-program entry point.
+ * Numbers must be separated by ,, followed by a space.
+ * Numbers should be printed in ascending order.
+ * You can only use the putchar function (every other function (printf,
+ * puts, etc…) is forbidden).
+ * All your code should be in the main function.
+ * You can only use putchar four times maximum in your code.
+ * You are not allowed to use any variable of type char.
  *
- *   *Return:0 success, non zero error.
- *
- *    
- */
+ * Return: 0.
+*/
+
 int main(void)
 {
-	  int num;
+int i;
 
-  	  for (num = '0'; num <= '9'; num++)
-   {
-            putchar(num);
-
-	   if (num == '9')
-	   {
-	   break;
-	    }
-	   else {
-                        
-           putchar(',');
-
-           putchar(' ');
-
-			      }
-
-			      }
-
-	   putchar('\n');
-
-	        return (0);
-
-}
+	for (i = '0'; i <= '9'; ++i)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+return (0);
+}}
