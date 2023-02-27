b2453e857998705b34d62d25ca2c55c6d0eab8af
laytoshcode <laytoshtwo@yahoo.com>
2023-02-26 16:02:54 -0800

    task1

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower-check if character is lowercase
+* @c:character to check if it islowercase
+* Return:1 if lowercase,0 if not
+*/
+int _islower(int c)
+{
+     if (c >= 'a' && c <= 'z')
+	   return (1);
+     else
+	  return (0);			
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*abs-computes  the absolute value of an integer.
+*@a:the integer to compute its absolute value.
+*iReturn:returns the abs value 
+ */ 
+int _abs(int t)
+
+{
+	if (t >= 0)
+		
+		return (t);
+	else 
+		return(-t);
+}
