d75fea1bb4c6910b0b7d068a6b14157ca938d35a
laytoshcode <laytoshtwo@yahoo.com>
2023-03-06 14:14:46 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+
 /**
- *  *main -is the entry point for the program.
- *   *
- *    *Return:zero if no error, Non-zero value  if errors.
- *     */
+ * main - Entry point
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0
+*/
+
 int main(void)
 {
-		printf("Size of a char: %ld byte(s)\n", sizeof(char));
-			printf("Size of an int: %ld byte(s)\n", sizeof(int));
-				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %ld byte(s)\n", sizeof(float));
-							return (0);
+	char e;
+	int f;
+	long int b;
+	long long int c;
+	float d;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(e));
+	printf("Size of an int: %d byte(s)\n", sizeof(f));
+	printf("Size of a long int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a float: %d byte(s)\n", sizeof(d));
+
+	return (0);
 }
 
