a85da2d781b2c97962f95b20a001703c2505ae24
laytoshcode <laytoshtwo@yahoo.com>
2023-03-06 14:33:24 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
 /**
- *  * main-entry point for the program.
- *   *
- *    * Return:0 if no errors, return non zero if errors
- *     */
+ * main - Entry point
+ *
+ * Use the function printf.
+ * You are not allowed to use the function puts.
+ * Your program should return 0.
+ * Your program should compile without warning when using the -Wall,
+ * gcc option.
+ *
+ * Return: 0
+*/
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
