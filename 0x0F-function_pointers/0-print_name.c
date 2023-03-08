77749f8e2639ee32e8d680a23029a4317bc73a02
laytoshcode <laytoshtwo@yahoo.com>
2023-03-08 01:47:27 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
- * print_name - prints a name
- * @name : name to print
- * @f: pointer to the printing function
+ * print_name - function that prints a name.
+ * @name: pointer to name.
+ * @f: pointer to function.
+ *
+ * Return: No return.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		 return;
-	f(name);
+	if (name && f)
+		f(name);
 }
