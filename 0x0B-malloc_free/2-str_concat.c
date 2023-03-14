a1d13573cbb58a17b09ae4330371f45624294189
laytoshcode <laytoshtwo@yahoo.com>
2023-03-14 04:11:26 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,77 +1,47 @@
-#include "main.h"
-		
+#include <stdio.h>
 #include <stdlib.h>
-		
+#include "main.h"
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-*/
-		
+ * str_concat - function that concatenates two strings.
+ * @s1: pointer to the null-terminated byte string to append to.
+ * @s2: pointer to the null-terminated byte string to copy from.
+ *
+ * Return: NULL (fail) or a pointer to newly allocated space in memory
+ * which contains the contents of s1, followed by the contents of s2,
+ * and null-terminated (Success).
+ */
+
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;	
+	char *newStr;
+	unsigned int i, j, k, limit;
 
-		
 	if (s1 == NULL)
-		
 		s1 = "";
-		
 	if (s2 == NULL)
-		
 		s2 = "";
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-	
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
 
-		
-	if (conct == NULL)
-		
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	newStr = malloc(sizeof(char) * (i + j + 1));
+
+	if (newStr == NULL)
+	{
+		free(newStr);
 		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
 	}
-		
-	while (s2[ci] != '\0')
-		
-	{
-		
-		conct[i] = s2[ci];
-i++, ci++;
-		
-	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
-}
 
+	for (k = 0; k < i; k++)
+		newStr[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		newStr[k] = s2[j];
+
+	return (newStr);
+}
