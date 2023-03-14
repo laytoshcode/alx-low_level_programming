f373aa0835495e4f0cf2a5868189480538c5de74
laytoshcode <laytoshtwo@yahoo.com>
2023-03-14 05:16:18 -0700

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,61 +1,37 @@
-#include <stdlib.h>
-		
 #include "main.h"
-		
+#include <stdlib.h>
 
-		
 /**
-		
- * *array_range - creates an array of integers
-		
- * @min: minimum range of values stored
-		
- * @max: maximum range of values stored and number of elements
-		
+ * array_range - function that creates an array of integers.
+ * @min: minimum integer in array.
+ * @max: maximum integer in array.
  *
-		
- * Return: pointer to the new array
-		
+ * The array created should contain all the values from min (included),
+ * to max (included), ordered from min to max.
+ * If min > max, return NULL.
+ * If malloc fails, return NULL.
+ *
+ * To avoid a memory leak, the returned pointer must be deallocated,
+ * with free() or realloc() but realloc is forbidden here..
+ *
+ * Return: A pointer to the newly created array (success).
  */
-		
+
 int *array_range(int min, int max)
-		
 {
-		
-	int *ptr;
-		
+	int *p;
 	int i, size;
-		
 
-		
+	size = (max - min) + 1;
+
 	if (min > max)
-		
-return (NULL);
-		
-
-		
-	size = max - min + 1;
-		
-
-		
-	ptr = malloc(sizeof(int) * size);
-		
-
-		
-	if (ptr == NULL)
-		
 		return (NULL);
-		
 
-		
-	for (i = 0; min <= max; i++)
-		
-		ptr[i] = min++;
-		
+	p = malloc(size * sizeof(*p));
+	if (p == NULL)
+		return (NULL);
 
-		
-	return (ptr);
-		
+	for (i = 0; i < size && min <= max; i++, min++)
+		*(p + i) = min; /* p[i] */
+	return (p);
 }
-
-
